82cbbd4b607feae226c5438033f06266726a5530
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-14 13:32:09 -0300

    adição da utilizacao de arquivos estaticos

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 //Express irá utilizar o EJS como View Engine.
 app.set('view engine', 'ejs');
+//Express irá utilizar arquivos estáticos.
+app.use(express.static('public'));
 
 app.get("/", (req, res) => {
     res.render("index")
